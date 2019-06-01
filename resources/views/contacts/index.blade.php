3bfcfd42a16fa331505088889be38298b09cacf7
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2019-06-01 14:03:07 -0300

    Cria a pagina inicial

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -4,12 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ str_replace('-', ' de ', config('app.name', 'Laravel')) }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <style>
             html, body {
                 background-color: #fff;
@@ -48,6 +49,10 @@
                 font-size: 84px;
             }
 
+            .subtitle {
+                font-size: 24px;
+            }
+
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -65,34 +70,30 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ str_replace('-', ' de ', config('app.name', 'Laravel')) }}
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="subtitle m-b-md">
+                    Um exemplo básico de um projeto Laravel
                 </div>
+
+                <div class="flex-center">
+                    <div class="btn-group" role="group">
+                        <a href="#" type="button" class="btn btn-light">Visualizar mensagens</a>
+                        <a href="{{ url('contatos/create') }}" type="button" class="btn btn-light">Enviar mensagem</a>
+                    </div>
+                </div>
+
+                <div class="flex-center">
+                    <div class="links">
+                        <a href="https://laravel.com/docs">Documentação</a>
+                        <a href="https://laracasts.com">Laracasts</a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </body>
