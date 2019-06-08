1b8353e61eed379d0d44f2cf764f802704fb1a70
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2019-06-08 11:18:28 -0300

    Altera a view da listagem de mensagens

--- resources/views/contacts/list.blade.php
+++ resources/views/contacts/list.blade.php
@@ -1,48 +1,49 @@
-@extends('layouts.app')
+@extends('layouts.form')
 
-@section('app-css')
+<!-- @section('app-css')
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-@endsection
+@endsection -->
 
-@section('body')
-<div class="flex-center position-ref full-height">
-
-    <div class="content">
-        <div class="title m-b-md">
-            Lista de Mensagens
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                @if(count($contacts) === 0)
+                <div class="card-header">Não há Mensagens</div>
+                @else
+                <div class="card-header">Lista de Mensagens</div>
+                <div class="card-body">
+                    <div class="form-group row">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Telefone</th>
+                                    <th scope="col">E-mail</th>
+                                    <th scope="col">Assunto</th>
+                                    <th scope="col">Mensagem</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($contacts as $contact)
+                                <tr>
+                                    <td scope="row">{{ $contact->id }}</td>
+                                    <td>{{ $contact->name }}</td>
+                                    <td>{{ $contact->phone }}</td>
+                                    <td>{{ $contact->email }}</td>
+                                    <td>{{ $contact->subject->description }}</td>
+                                    <td>{{ $contact->message->message }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                @endif
+            </div>
         </div>
-
-        <div class="flex-center">
-            @if(count($contacts) === 0)
-                Não há mensagens
-            @else
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Telefone</th>
-                        <th scope="col">E-mail</th>
-                        <th scope="col">Assunto</th>
-                        <th scope="col">Mensagem</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($contacts as $contact)
-                        <tr>
-                            <td scope="row">{{ $contact->id }}</td>
-                            <td>{{ $contact->name }}</td>
-                            <td>{{ $contact->phone }}</td>
-                            <td>{{ $contact->email }}</td>
-                            <td>{{ $contact->subject->description }}</td>
-                            <td>{{ $contact->message->message }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            @endif
-        </div>
-
     </div>
 </div>
 @endsection
