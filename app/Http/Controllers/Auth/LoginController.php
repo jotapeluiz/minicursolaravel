71e082d985b15470b7426c46a3d040f8e8982391
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2019-06-09 16:32:05 -0300

    Ajusta as rotas de autenticacao

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/contatos/create';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
